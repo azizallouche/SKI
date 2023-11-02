412dedfac3f7c4b52e6990a583ace7d95b3cccd4
Louay-Guetat <louay.guetat@esprit.tn>
2023-11-02 16:15:57 +0100

    first commit

---
+++ src/main/java/tn/esprit/SkiStationProject/services/SubscriptionServicesImpl.java
@@ -0,0 +1,76 @@
+package tn.esprit.SkiStationProject.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import tn.esprit.SkiStationProject.entities.Skier;
+import tn.esprit.SkiStationProject.entities.Subscription;
+import tn.esprit.SkiStationProject.entities.enums.TypeSubscription;
+import tn.esprit.SkiStationProject.repositories.SkierRepository;
+import tn.esprit.SkiStationProject.repositories.SubscriptionRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class SubscriptionServicesImpl implements ISubscriptionServices{
+
+    private final SubscriptionRepository subscriptionRepository;
+
+    private final SkierRepository skierRepository;
+
+    @Override
+    public Subscription addSubscription(Subscription subscription) {
+        switch (subscription.getTypeSub()) {
+            case ANNUAL:
+                subscription.setEndDate(subscription.getStartDate().plusYears(1));
+                break;
+            case SEMESTRIEL:
+                subscription.setEndDate(subscription.getStartDate().plusMonths(6));
+                break;
+            case MONTHLY:
+                subscription.setEndDate(subscription.getStartDate().plusMonths(1));
+                break;
+        }
+        return subscriptionRepository.save(subscription);
+    }
+
+    @Override
+    public Subscription updateSubscription(Subscription subscription) {
+        return subscriptionRepository.save(subscription);
+    }
+
+    @Override
+    public Subscription retrieveSubscriptionById(Long numSubscription) {
+        return subscriptionRepository.findById(numSubscription).orElse(null);
+    }
+
+    @Override
+    public Set<Subscription> getSubscriptionByType(TypeSubscription type) {
+        return subscriptionRepository.findByTypeSubOrderByStartDateAsc(type);
+    }
+
+    @Override
+    public List<Subscription> retrieveSubscriptionsByDates(LocalDate startDate, LocalDate endDate) {
+        return subscriptionRepository.getSubscriptionsByStartDateBetween(startDate, endDate);
+    }
+
+    @Override
+    public void retrieveSubscriptions() {
+        for (Subscription sub: subscriptionRepository.findDistinctOrderByEndDateAsc()) {
+            Skier   aSkier = skierRepository.findBySubscription(sub);
+            log.info(sub.getId() + " | "+ sub.getEndDate().toString()
+                    + " | "+ aSkier.getFirstName() + " " + aSkier.getLastName());
+        }
+    }
+
+    public void showMonthlyRecurringRevenue() {
+        Float revenue = subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.MONTHLY)
+                + subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.SEMESTRIEL)/6
+                + subscriptionRepository.recurringRevenueByTypeSubEquals(TypeSubscription.ANNUAL)/12;
+        log.info("Monthly Revenue = " + revenue);
+    }
+}
