4671c060475eca8a6ba16e4303310e2879a7ffaa
FarahBK <61430359+FarahBK@users.noreply.github.com>
2023-11-09 23:48:49 +0100

    Update InstructorServicesImplTest.java
    pp

--- src/test/java/tn/esprit/SkiStationProject/InstructorServicesImplTest.java
+++ src/test/java/tn/esprit/SkiStationProject/InstructorServicesImplTest.java
@@ -33,7 +33,7 @@ import static org.mockito.Mockito.*;
             MockitoAnnotations.initMocks(this);
 
             // Test data setup
-            //ngrokkk
+            //ngrokkkk
             Instructor instructor1 = new Instructor();
             instructor1.setFirstName("farah");
             instructor1.setLastName("braiki");
