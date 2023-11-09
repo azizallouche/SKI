009bfdf227ca6c7061d1a731ce6acb5fcddaf055
mohamedalimzoughi <mohamedali.mzoughi@esprit.tn>
2023-11-09 14:28:20 +0100

    testes

--- src/test/java/tn/esprit/SkiStationProject/SubscriptionMockImpTest.java
+++ src/test/java/tn/esprit/SkiStationProject/SubscriptionMockImpTest.java
@@ -1,12 +1,15 @@
 package tn.esprit.SkiStationProject;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;  // Add this import
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;  // Add this import
+import org.springframework.test.context.junit4.SpringRunner;  // Add this import
 import tn.esprit.SkiStationProject.entities.Subscription;
 import tn.esprit.SkiStationProject.entities.enums.TypeSubscription;
 import tn.esprit.SkiStationProject.repositories.SubscriptionRepository;
@@ -15,11 +18,14 @@ import tn.esprit.SkiStationProject.services.SubscriptionServicesImpl;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+@RunWith(SpringRunner.class)  // Add this annotation
+@SpringJUnitConfig(classes = SubscriptionMockImpTest.class)  // Add this annotation
 @SpringBootTest
 @ActiveProfiles("test")
 public class SubscriptionMockImpTest {
@@ -28,40 +34,13 @@ public class SubscriptionMockImpTest {
 
     @Mock
     private SubscriptionRepository subscriptionRepository;
-    @Test
-    public void shouldRetrieveSubscriptionById() {
-        // Create sample courses
-        Subscription subscription1 = new Subscription(LocalDate.of(2023, 11, 6), LocalDate.of(2023, 12, 6), 220.0f, TypeSubscription.MONTHLY);
 
-        Subscription sub = subscriptionServices.addSubscription(subscription1);
-
-        // Call the service method to retrieve the subscription by ID
-        when(subscriptionRepository.findById(sub.getId())).thenReturn(java.util.Optional.of(sub));
-
-        // Call the service method to retrieve the subscription by ID
-        Subscription result = subscriptionServices.retrieveSubscriptionById(sub.getId());
-
-        // Assert that the result matches the sample subscription
-        assertEquals(sub, result);
-
-        // Print a success message
-        System.out.println("shouldRetrieveSubscriptionById succeeded!");
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
     }
-    @Test
-    public void shouldRetrieveSubscriptionByType() {
-        List<Subscription> subscriptions = new ArrayList<>();
-        subscriptions.add(new Subscription(LocalDate.of(2023, 11, 6), LocalDate.of(2023, 12, 6), 220.0f, TypeSubscription.MONTHLY));
-        subscriptions.add(new Subscription(LocalDate.of(2023, 11, 6), LocalDate.of(2023, 12, 6), 220.0f, TypeSubscription.MONTHLY));
 
-        when(subscriptionRepository.findByTypeSub(TypeSubscription.MONTHLY)).thenReturn(subscriptions);
 
-        Set<Subscription> result = subscriptionServices.getSubscriptionByType(TypeSubscription.MONTHLY);
-
-        assertEquals(subscriptions, result);
-
-        // Print a success message
-        System.out.println("shouldRetrieveSubscriptionByType succeeded!");
-    }
     @Test
     public void shouldRetrieveSubscriptionsByDates() {
         List<Subscription> subscriptions = new ArrayList<>();
@@ -74,7 +53,6 @@ public class SubscriptionMockImpTest {
 
         assertEquals(subscriptions, result);
 
-        // Print a success message
         System.out.println("shouldRetrieveSubscriptionsByDates succeeded!");
     }
 }
